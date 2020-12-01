509775103ba9bcb0c6e2604bf640acab39d3dd86
John T Porvaznik <scurvyjtp@gmail.com>
2020-12-01 18:01:41 -0500

    AOC Day 01 Part 1.

---
+++ 01a.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "strconv"
+)
+
+func check (e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func fileToArray (fn string) []int {
+    var l []int
+
+    file, err := os.Open(fn)
+    check(err)
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines)
+    for scanner.Scan() {
+        n, _ := strconv.Atoi(scanner.Text())
+        l = append(l, n)
+    }
+
+    file.Close()
+    return l
+}
+
+func checkArray(vals []int) {
+    base := vals[0]
+    l := len(vals)
+
+    for i :=1; i< l; i++ {
+        if base + vals[i] == 2020 {
+            fmt.Printf("%d * %d = %d\n", base, vals[i], (base * vals[i]))
+            os.Exit(1)
+        }
+    }
+}
+
+func main() {
+    var fn = "input/01.txt"
+    vals := fileToArray(fn)
+
+    for n, _ := range vals {
+        checkArray(vals[n:])
+    }
+}
