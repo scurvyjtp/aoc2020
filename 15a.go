5adc85cf2cc4428690d8f8b9a31c33f1cc41ee0a
scurvyjtp <scurvyjtp@gmail.com>
2020-12-24 10:20:53 -0500

    AoC Day 15 part 1.

---
+++ 15a.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	//"bufio"
+	"fmt"
+	//"os"
+	"strconv"
+    "strings"
+)
+
+func sToi (in []string) []int {
+    var out []int
+    for _, v := range in {
+        t, _ := strconv.Atoi(v)
+        out = append(out,t)
+    }
+    return out
+}
+
+func getAge(inArr []int ) int {
+    next := inArr[len(inArr)-1]
+    for i := len(inArr)-2; i >=0; i-- {
+        if inArr[i] == next {
+            return (len(inArr) - (i+1))
+        }
+    }
+    return 0
+}
+
+
+func main() {
+	input := "1,17,0,10,18,11,6"
+    turns := 2020
+    orig := strings.Split(input, ",")
+    origI := sToi(orig)
+
+    for count := len(origI); count != turns; count++ {
+        origI = append(origI, getAge(origI))
+        fmt.Printf("Answer: %d\n", origI)
+    }
+
+    fmt.Printf("Answer: %d\n", origI[len(origI)-1])
+
+}
+
